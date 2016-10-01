ccbad1cf8e251af9cfd85b9fc1ac54492dc2d50c
fntsrlike <fntsrlike@gmail.com>
2016-10-01 10:14:32 +0800

    Change description of morse codec on textarea placeholder

--- src/components/Coder.vue
+++ src/components/Coder.vue
@@ -1,19 +1,18 @@
 <template>
-  <section id="coder" class="section is-medium">
+  <section id="coder" class="section">
     <div class="container">
       <div class="heading">
-        <h1 class="title is-2">Coder</h1>
-        <h2 class="subtitle is-4">編碼器</h2>
+        <h1 class="title is-2">Codec</h1>
+        <h2 class="subtitle is-4">編/解碼器</h2>
       </div>
       <section>
-        <p class="control">
+        <div class="control">
           <textarea id="input" class="textarea is-medium"
             v-bind:class="{ 'is-danger': !isInputValid }"
             v-model="input" v-on:keyup="JudgeInput"
             placeholder="{{placeholder}}"></textarea>
           <span class="help is-danger" v-show="!isInputValid">欄位不得為空</span>
-        </p>
-
+        </div>
         <div class="control">
           <button class="button is-primary" @click="Encode" v-bind:class="{ 'is-disabled': !isInputChars}">編碼 / Encode</button>
           <button class="button is-primary" @click="Decode" v-bind:class="{ 'is-disabled': !isInputMorse}">解碼 / Decode</button>
@@ -77,7 +76,7 @@ new Clipboard('.copy.button')
 export default {
   data: function () {
     return {
-      placeholder: '明文只限定摩斯電碼支援字元。若輸入電碼，字元電碼之間記得要有一格空白。',
+      placeholder: '您可以在文字框裡輸入本頁下方所列出的英數字元與符號、摩斯訊號符號進行編、解碼。當您輸入的文字、符號是符合規則的內容時，文字框下方的編碼、解碼按鈕就可以點擊。',
       input: '',
       output: '',
       isInputValid: true,
