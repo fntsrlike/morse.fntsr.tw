f52b841b6cfc624ef960c6a81bb8415e33458edd
fntsrlike <fntsrlike@gmail.com>
2016-11-21 23:26:14 +0800

    Move code table style to code tables page

--- src/components/CodeTable.vue
+++ src/components/CodeTable.vue
@@ -18,46 +18,4 @@
 </script>
 
 <style>
-  .code {
-    flex: none;
-    margin: 0 0 1rem ;
-    padding: 0;
-    display: block;
-    font-family: monospace;
-  }
-
-  .code .heading {
-    display: block;
-    font-size: 11px;
-    letter-spacing: 1px;
-    margin-bottom: 5px;
-    text-transform: uppercase;
-    color: #ff3860;
-  }
-
-  .code .title {
-    color: #363636;
-    font-size: 28px;
-    line-height: 1;
-    font-weight: 300;
-    word-break: break-word;
-  }
-
-  @media screen and (max-width: 512px) {
-    .code {
-      width: 25%;
-    }
-  }
-
-  @media screen and (min-width: 512px) {
-    .code {
-      width: 16.66667%;
-    }
-  }
-
-  @media screen and (min-width: 1024px) {
-    .code {
-      width: 12.5%;
-    }
-  }
 </style>
--- src/components/CodeTables.vue
+++ src/components/CodeTables.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container">
       <div class="heading">
-        <h2 class="title is-2">Table</h1>
+        <h2 class="title is-2">Table</h2>
         <h2 class="subtitle is-4">
           摩斯電碼對照表
         </h2>
@@ -49,4 +49,47 @@ export default {
 </script>
 
 <style>
+  .code {
+    flex: none;
+    margin: 0 0 1rem ;
+    padding: 0;
+    display: block;
+    font-family: monospace;
+    cursor: pointer;
+  }
+
+  .code .heading {
+    display: block;
+    font-size: 11px;
+    letter-spacing: 1px;
+    margin-bottom: 5px;
+    text-transform: uppercase;
+    color: #ff3860;
+  }
+
+  .code .title {
+    color: #363636;
+    font-size: 28px;
+    line-height: 1;
+    font-weight: 300;
+    word-wrap: break-word;
+  }
+
+  @media screen and (max-width: 512px) {
+    .code {
+      width: 25%;
+    }
+  }
+
+  @media screen and (min-width: 512px) {
+    .code {
+      width: 16.66667%;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .code {
+      width: 12.5%;
+    }
+  }
 </style>
