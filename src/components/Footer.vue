f4810385e466df9a7c4a70f13014b6b15dcd6e9e
fntsrlike <fntsrlike@gmail.com>
2016-11-22 18:26:54 +0800

    Add target attribute on links of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,18 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          <strong>Morse Code</strong> by <a href="http://fntsr.tw">Ruoshi Lin</a>.
-          Powered by <a href="http://bulma.io">BULMA</a> & <a href="https://vuejs.org">Vue.js</a>
+          <strong>Morse Code</strong> by
+          <a href="http://fntsr.tw" target="_blank">Ruoshi Lin</a>.
+          Powered by
+          <a href="http://bulma.io" target="_blank">BULMA</a> &
+          <a href="https://vuejs.org" target="_blank">Vue.js</a>
           <br/>
           The source code is licensed
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
           is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
         </p>
         <p>
-          <a class="icon" href="https://github.com/fntsrlike/Morse-Code">
+          <a class="icon" href="https://github.com/fntsrlike/Morse-Code" target="_blank">
             <i class="fa fa-github"></i>
           </a>
         </p>
